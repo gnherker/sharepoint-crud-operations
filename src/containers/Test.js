08af1431348fe0a2c5a000f47d0d087782acce10
Guilherme <gnherker@gmail.com>
2019-09-01 21:50:55 -0300

    another test

--- src/containers/Test.js
+++ src/containers/Test.js
@@ -17,7 +17,6 @@ export default function Test(props) {
   const [orderBy, setOrderBy] = useState("Title");
   const [order, setOrder] = useState("asc");
 
-
   function getFormDigest() {
     fetch(
       "http://localhost:8080/_api/contextinfo",
